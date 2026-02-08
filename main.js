4462cb482e45e76578961ba5d24239e4e16b26f8
Ben Spain <ben@thespains.net>
2026-02-08 17:15:55 -0600

    feat: media keys

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
 const path = require("path");
+let mainWindow;
+
+const mediaShortcuts = [
+  { accelerator: "VolumeMute", code: "MediaMute", label: "Mute" },
+  { accelerator: "VolumeDown", code: "MediaVolumeDown", label: "Vol-" },
+  { accelerator: "VolumeUp", code: "MediaVolumeUp", label: "Vol+" },
+  { accelerator: "MediaPlayPause", code: "MediaPlayPause", label: "Play" },
+  { accelerator: "MediaPreviousTrack", code: "MediaPreviousTrack", label: "Prev" },
+  { accelerator: "MediaNextTrack", code: "MediaNextTrack", label: "Next" }
+];
+
+const registerMediaShortcuts = (win) => {
+  mediaShortcuts.forEach(({ accelerator, code, label }) => {
+    globalShortcut.register(accelerator, () => {
+      if (!win || win.isDestroyed()) return;
+      win.webContents.send("media-key", { code, label });
+    });
+  });
+};
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -16,17 +35,24 @@ const createWindow = () => {
 
   win.setMenuBarVisibility(false);
   win.loadFile(path.join(__dirname, "index.html"));
+  return win;
 };
 
 app.whenReady().then(() => {
   Menu.setApplicationMenu(null);
-  createWindow();
+  mainWindow = createWindow();
+  registerMediaShortcuts(mainWindow);
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+      mainWindow = createWindow();
+      registerMediaShortcuts(mainWindow);
     }
   });
+});
+
+app.on("will-quit", () => {
+  globalShortcut.unregisterAll();
 });
 
 app.on("window-all-closed", () => {
--- preload.js
+++ preload.js
@@ -1,5 +1,10 @@
-const { contextBridge } = require("electron");
+const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("keyboardTester", {
-  version: "1.0.0"
+  version: "1.0.0",
+  onMediaKey: (callback) => {
+    ipcRenderer.on("media-key", (_event, payload) => {
+      callback(payload);
+    });
+  }
 });
